13cc318c0c527d183ef92d4fec709f36d0e41725
Milan <milanm1120@gmail.com>
2021-08-18 11:22:47 -0400

    commit tree 05817208d3e67f2dadd2f1a533806ae3ff2d27b7

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,33 @@
+class CashRegister
 
+    attr_accessor :total, :discount, :items
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def add_item(item, price, quantity = 1)        #set optional quantity for a minimum of 1
+        quantity.times do
+          @items << item
+        end
+        @last_total = @total
+        @total += price * quantity                    #minimal default quantity of 1, aditional quantity will be added to the price.
+      end
+    
+      def apply_discount
+        if @discount > 0
+          @total = (@total - @total * @discount.to_f / 100).to_i               #to_f makes it a floating number
+          return "After the discount, the total comes to $#{@total.to_i}."
+        else
+          return "There is no discount to apply."
+        end
+      end
+    
+      def void_last_transaction
+        @total = @last_total
+      end
+    
+    
+    end
